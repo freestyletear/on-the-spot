60aa8a6d2c51b4c371ff7d667ce6fbbb8bc2e152
freestyletear <freestyletear@gmail.com>
2021-09-12 13:35:47 +0300

    attempt commit correction

--- config.py
+++ config.py
@@ -1,21 +1,29 @@
-
 import os
 
-class Config:
 
-    NEWS_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+class Config:
+    """
+    General configuration parent class
+    """
+    NEWS_API_SOURCE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
+    NEWS_API_ARTICLES_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    @staticmethod
+    def init_app(app):
+        pass
 
 
 class ProdConfig(Config):
     pass
 
 
-class DevConfig(Config):
+class DevelopmentConfig(Config):
     DEBUG = True
 
-config_options = {
-'development':DevConfig,
-'production':ProdConfig
+
+config_map = {
+    'development': DevelopmentConfig,
+    'production': ProdConfig
 }
